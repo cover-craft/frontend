62d518c2ca815139ab66606d3438ce340dcfaba6
sunjungAn <dkstjswjd83@naver.com>
2023-06-19 23:05:04 +0900

    close: #6 init next.js template

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div className="container">
+            <span>init Next.js</span>
+        </div>
+    );
+}

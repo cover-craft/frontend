014fb06e9bcbe96a31651db753de1edb93010dfc
sunjungAn <dkstjswjd83@naver.com>
2023-06-27 22:51:44 +0900

    fix: image ssr error

--- components/ImageSection.tsx
+++ components/ImageSection.tsx
@@ -2,7 +2,6 @@ import banner1 from "../public/images/home/home_image_banner1";
 import { Flex } from "@chakra-ui/react";
 import Image from "next/image";
 const ImageSection = () => {
-    const reversed_banner = banner1.reverse();
     return (
         <Flex flexDir={"column"} height={"100vh"} gap={10} bg="black">
             <Flex gap={10} mt={"auto"}>
@@ -11,7 +10,7 @@ const ImageSection = () => {
                 })}
             </Flex>
             <Flex gap={10}>
-                {reversed_banner.map((img, index) => {
+                {banner1.map((img, index) => {
                     return (
                         <Image key={`${img}_${index}`} src={img} alt={"생성 이미지 예시"} width={130} loading="lazy" unoptimized></Image>
                     );

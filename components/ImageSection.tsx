eb643801ff04e12769e1497c3b9ffbda39f2977f
sunjungAn <dkstjswjd83@naver.com>
2023-06-27 18:13:50 +0900

    feat: image size 조절

--- components/ImageSection.tsx
+++ components/ImageSection.tsx
@@ -7,12 +7,23 @@ const ImageSection = () => {
         <Flex flexDir={"column"} height={"100vh"} gap={10} bg="black">
             <Flex gap={10} mt={"auto"}>
                 {banner1.map((img, index) => {
-                    return <Image key={index} src={img} alt={"생성 이미지 예시"} width={200}></Image>;
+                    return (
+                        <Image
+                            key={index}
+                            src={img}
+                            alt={"생성 이미지 예시"}
+                            layout="responsive"
+                            sizes="(min-width: 500px) 200px, 150px"
+                            unoptimized
+                        ></Image>
+                    );
                 })}
             </Flex>
             <Flex gap={10}>
                 {reversed_banner.map((img, index) => {
-                    return <Image key={`${img}_${index}`} src={img} alt={"생성 이미지 예시"} width={130} loading="lazy"></Image>;
+                    return (
+                        <Image key={`${img}_${index}`} src={img} alt={"생성 이미지 예시"} width={130} loading="lazy" unoptimized></Image>
+                    );
                 })}
             </Flex>
         </Flex>
